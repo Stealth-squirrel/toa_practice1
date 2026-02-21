65aadd16b1e83ca016ea8435e6a979da06a0d306
Anastasia Kudryavtseva <nkudr.1823@gmail.com>
2026-02-21 20:46:50 +0300

    Update func.py

--- func.py
+++ func.py
@@ -1,5 +1,5 @@
 def is_unstable(cats_prefs, dogs_prefs, cat_dog_pairs, pair_checked):
-    # Создаем словари рангов 
+    # Создаем словари рангов
     cat_rank = {}
     for cat, prefs in cats_prefs.items():
         cat_rank[cat] = {dog: i for i, dog in enumerate(prefs)}
@@ -9,21 +9,25 @@ def is_unstable(cats_prefs, dogs_prefs, cat_dog_pairs, pair_checked):
         dog_rank[dog] = {cat: i for i, cat in enumerate(prefs)}
     # Словарь, где данные паросочетания кошка-собака представлены в виде собака-кошка
     dog_cat_pairs = {dog: cat for cat, dog in cat_dog_pairs.items()}
-    
-    # Проверка пар на неустойчивость
-    for cat in list(pair_checked.keys()):
+
+    # Проверка пары на неустойчивость
+    for cat, dog in pair_checked.items():
         current_dog = cat_dog_pairs[cat]
-        dog = pair_checked[cat]
         current_cat = dog_cat_pairs[dog]
+
+        # Проверка пары на существование
+        if current_cat == cat and current_dog == dog:
+            return f'Пара {cat} - {dog} уже установлена'
+            continue
 
         # 1. Кошка предпочитает эту собаку своей текущей собаке
         cat_prefers_dog = cat_rank[cat][dog] < cat_rank[cat][current_dog]
 
         # 2. Собака предпочитает эту кошку своей текущей кошке
         dog_prefers_cat = dog_rank[dog][cat] < dog_rank[dog][current_cat]
-        
+
         # Если оба условия верны -> найдена нестабильная пара
         if cat_prefers_dog and dog_prefers_cat:
-            print(f"Пара {cat} - {dog} неустойчивая")         
-        else: 
-          print(f"Пара {cat} - {dog} устойчивая")
+            return f"Пара {cat} - {dog}: неустойчивое"
+        else:
+            return f"Пара {cat} - {dog}: устойчивое"
